a985b8d062e0c933b4cc076110185af185b391fb
Filipi <filipi.pereira.neves@gmail.com>
2022-10-17 21:23:31 -0300

    segundo commit

---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+/*------------ SHOW MENU-------------*/
+const showMneu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    /*valida se se existe as variaveis*/
+    if (toggle && nav) {
+        /*aqui adiciono a class show-menu na div com a class nav__menu*/
+        toggle.addEventListener('click', () => {
+            /*adicina a class show-menu na div que tem o id nav-menu */
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMneu('nav-toggle', 'nav-menu')
+
+/*------------ REMOVER MENU MOBILE-------------*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    /*Quando um link for clicado a class show-menu será removido*/
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== DEIXA O LINK CLICADO COM A CLASS ACTIVE-LINK ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active-link'))
+        this.classList.add('active-link')
+    }
+}
+
+linkColor.forEach(L=> L.addEventListener('click', colorLink))
+
+/*------------BOX SHADOW NO HEADER-------------*/
+function scrollHeader(){
+    const scrollHeader = document.getElementById('header');
+    // Quando o scroll tiver um altura maior que 200 será adiconado a class scroll-header na tag Header
+    if(this.scrollY >= 200) scrollHeader.classList.add('scroll-header'); else scrollHeader.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*------------SHOW SCROLL TOP------------- */
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // Quando o scroll tiver um altura maior que 560 será adiconado a class scroll-top
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+/*------------Cardapio------------- */
+
+function abrirWhatsapp() {
+
+    var prato = '';
+
+    var polvo = document.getElementById("polvo");
+    var nhocchicamarao = document.getElementById("nhocchicamarao");
+    var camaraoempanado = document.getElementById("camaraoempanado");
+    var filedepeixe = document.getElementById("filedepeixe");
+
+    if (polvo.checked){
+        prato += polvo.value + " ";
+    }
+    
+    if (nhocchicamarao.checked) {
+        prato += nhocchicamarao.value + " ";
+    }
+
+    if (camaraoempanado.checked) {
+        prato += camaraoempanado.value + " ";
+    }
+
+    if (filedepeixe.checked) {
+        prato += filedepeixe.value + " ";
+    }
+  
+    var url = "https://web.whatsapp.com/send/?phone=5522997397377&text=" // Meu numero
+      + "*DADOS*" + "%0a" // Mensagem personalizada
+      + "%0a" // Quebra de linha
+      + "*Prato: " + prato + "%0a"
+      
+      + "&type=phone_number&app_absent=0"
+
+    window.open(url, '_blank').focus();
+  }
